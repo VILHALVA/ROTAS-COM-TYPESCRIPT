fe4854bf6394bb73ca3f803d0244ce354766482d
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-07-10 12:14:08 -0500

    Add files via upload

---
+++ CODIGO/dist/APP.js
@@ -0,0 +1,30 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') {
+        filePath = './public/home.html';
+    }
+    else {
+        filePath = './public' + req.url + '.html';
+    }
+    // Verificar se o arquivo existe
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            // Arquivo não encontrado, retornar erro 404
+            res.writeHead(404);
+            res.end("Arquivo não encontrado.");
+            return;
+        }
+        // Arquivo encontrado, servir conteúdo
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
+});
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`SERVIDOR RODANDO EM http://localhost:${PORT}/`);
+});
